ecfeaf4720f31f89e5d67d50bc3725977ca4b80a
Tomasz Tworek <tomasztworek27@gmail.com>
2020-05-18 17:21:58 +0200

    Initial commit - add basic dependencies, gitignore

---
+++ src/main/java/pl/endproject/offerscomparator/OffersComparatorApplication.java
@@ -0,0 +1,13 @@
+package pl.endproject.offerscomparator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffersComparatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OffersComparatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/endproject/offerscomparator/OffersComparatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.endproject.offerscomparator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OffersComparatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
