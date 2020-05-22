b3d4274aedfdb73f2711265386a83718509ecfb2
Tomasz Tworek <tomasztworek27@gmail.com>
2020-05-22 12:55:06 +0200

    Add test that displat scrapre items and validate returned pattern

--- src/test/java/pl/endproject/offerscomparator/apiOlx/api/OlxScraperTest.java
+++ src/test/java/pl/endproject/offerscomparator/apiOlx/api/OlxScraperTest.java
@@ -1,8 +1,12 @@
 package pl.endproject.offerscomparator.apiOlx.api;
 
 import org.junit.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class OlxScraperTest {
 
@@ -14,10 +18,33 @@ public class OlxScraperTest {
         Integer size = 5;
 
         //when
-        List<String> strings = OlxScraper.searchFor(searchingItem, size);
+        List<String> result = OlxScraper.searchFor(searchingItem, size);
 
         //then
-        assertThat(strings.size()).isEqualTo(5);
+        assertThat(result.size()).isEqualTo(5);
     }
-    
+
+    @Test
+    public void shouldDisplayCertainStringPatternAfterSearchingForItem() {
+        //given
+        String searchingItem = "Potop";
+        String patternString = ".*:.*";
+
+        //when
+        List<String> result = OlxScraper.searchFor(searchingItem, 5);
+        Pattern pattern = Pattern.compile(patternString);
+
+        //then
+        assertThat(pattern.matcher(result.get(0)).matches()).isEqualTo(true);
+    }
+
+    @Test
+    public void displayResultOfScraperItems(){
+        String searchingItem = "Potop";
+
+        List<String> result = OlxScraper.searchFor(searchingItem, 5);
+
+        System.out.println(result);
+    }
+
 }
