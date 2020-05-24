01fa69959b48ec310d2138d194ab9e9a962613db
Tomasz Tworek <tomasztworek27@gmail.com>
2020-05-24 12:38:47 +0200

    olx api #13 - add olxAdapter

---
+++ src/main/java/pl/endproject/offerscomparator/infrastructure/apiOlx/OlxAdapter.java
@@ -0,0 +1,49 @@
+package pl.endproject.offerscomparator.infrastructure.apiOlx;
+
+import org.springframework.stereotype.Component;
+import pl.endproject.offerscomparator.domain.Product;
+import pl.endproject.offerscomparator.domain.port.ProductRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class OlxAdapter implements ProductRepository {
+
+    @Override
+    public List<Product> findByPhrase(String phrase) {
+        //From Scraper To Dtos
+        List<ProductDto> productDtos = getProductsFromScraper(phrase);
+        //From Dtos To Domain
+        return mapToDomain(productDtos);
+    }
+
+    private List<ProductDto> getProductsFromScraper(String phrase) {
+        List<String> scrapedProducts = OlxScraper.searchFor(phrase, 10);
+        List<ProductDto> dtosProducts = new ArrayList<>();
+        for (String productFromScraper : scrapedProducts) {
+            ProductDto newProduct = convertStringProductFromScraperToProductDto(productFromScraper);
+            dtosProducts.add(newProduct);
+        }
+        return dtosProducts;
+    }
+
+    private ProductDto convertStringProductFromScraperToProductDto(String productFromScraper) {
+        String[] splitElements = productFromScraper.trim().split("::");
+        return ProductDto.builder()
+                .name(splitElements[0])
+                .price(splitElements[1])
+                .imageUrl(splitElements[2])
+                .url(splitElements[3])
+                .build();
+    }
+
+    private List<Product> mapToDomain(List<ProductDto> productDtos) {
+        return productDtos.stream()
+                .map(ProductDto::toDomain)
+                .collect(Collectors.toList());
+    }
+
+
+}
