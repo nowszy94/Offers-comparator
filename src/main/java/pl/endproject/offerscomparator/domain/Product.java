70dce501d1df828159e90b06f5b999a98eba08b4
Tomasz Tworek <tomasztworek27@gmail.com>
2020-05-24 12:34:09 +0200

    olx api #8 - small changes in domain, add builder and constructor, add Repository annotation

--- src/main/java/pl/endproject/offerscomparator/domain/Product.java
+++ src/main/java/pl/endproject/offerscomparator/domain/Product.java
@@ -1,10 +1,14 @@
 package pl.endproject.offerscomparator.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Product {
 
     private Long id;
